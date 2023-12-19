cf5ee1843c0b99966a8f2806a5c0170e5d6f2400
ariannacostanzo <costanzo.arianna@outlook.it>
2023-12-19 15:07:14 +0100

    added first ticket style

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,9 @@
 console.log('JS OK')
+
+// SCALETTA
+
+// 1. Raccogliere tutti gli elementi della pagina che mi servono
+// 2. Chiedere al passeggero il nome, l'età e il numero dei chilometri negli input
+// 3. Fare la logica dello sconto
+// 4. Stampare in pagina i risultati
+
